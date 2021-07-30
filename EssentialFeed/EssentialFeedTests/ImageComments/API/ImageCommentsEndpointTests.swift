8f9ecfa64caea43827391a14197e00e31a2067a5
Maciej Krolikowski <maciej.krolikowski@allegro.pl>
2021-07-30 15:44:38 +0200

    Avoid adding extra logic in tests. If we duplicated a logic which comes from production code, tests won't return failure when needed

--- EssentialFeed/EssentialFeedTests/ImageComments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments/API/ImageCommentsEndpointTests.swift
@@ -9,13 +9,21 @@ class ImageCommentsEndpointTests: XCTestCase {
 	func test_imageCommentsEndpoint_endpointURL() {
 		let baseURL = URL(string: "http://base-url.com")!
 
-		let sampleCommentsIds = [UUID(), UUID(), UUID()]
+		let sampleCommentsIds = [
+			UUID(uuidString: "123e4567-e89b-12d3-a456-426614174000")!,
+			UUID(uuidString: "321e4567-e89b-12d3-a456-426614174000")!,
+			UUID(uuidString: "132e4567-e89b-12d3-a456-426614174000")!
+		]
 
-		sampleCommentsIds.forEach { imageId in
+		let expectedURLs = [
+			URL(string: "http://base-url.com/v1/image/123E4567-E89B-12D3-A456-426614174000/comments")!,
+			URL(string: "http://base-url.com/v1/image/321E4567-E89B-12D3-A456-426614174000/comments")!,
+			URL(string: "http://base-url.com/v1/image/132E4567-E89B-12D3-A456-426614174000/comments")!
+		]
+
+		zip(sampleCommentsIds, expectedURLs).forEach { (imageId, expectedURL) in
 			let received = ImageCommentsEndpoint.get.url(baseURL: baseURL, imageId: imageId)
-			let expected = URL(string: "http://base-url.com/v1/image/\(imageId.uuidString)/comments")!
-
-			XCTAssertEqual(received, expected)
+			XCTAssertEqual(received, expectedURL)
 		}
 	}
 }
